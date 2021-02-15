473ced54939d82b851f5f7ccb1f21ebb8e0c31c7
TL-Garcia <tomas.bl.garcia@gmail.com>
2021-02-15 21:55:20 +0100

    Implement stream reader

--- read_it.js
+++ read_it.js
@@ -1,4 +1,11 @@
-const { Readable }
-
+const { Readable } = require('stream')
 const myStream = new Readable
+
+myStream.read = (buf) => {
+    buf.toString()
+}
+
+console.log(process.stdin)
+
+process.stdin.pipe(process.stdout)
 
